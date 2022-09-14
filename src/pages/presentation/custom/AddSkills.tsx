4b3ba00381b8ff0546b74aaf89fca41b793bf666
Luis Velito <lvelitoruiz>
2022-09-14 14:39:34 -0500

    categories and skills

--- src/pages/presentation/custom/AddSkills.tsx
+++ src/pages/presentation/custom/AddSkills.tsx
@@ -2,22 +2,189 @@ import React from 'react';
 import PageWrapper from '../../../layout/PageWrapper/PageWrapper';
 import Page from '../../../layout/Page/Page';
 import { userMenu } from '../../../menu';
-import Popovers from '../../../components/bootstrap/Popovers';
+import Card, { CardBody } from '../../../components/bootstrap/Card';
+import FormGroup from '../../../components/bootstrap/forms/FormGroup';
+import Input from '../../../components/bootstrap/forms/Input';
+import Checks, { ChecksGroup } from '../../../components/bootstrap/forms/Checks';
+import Icon from '../../../components/icon/Icon';
+import Button from '../../../components/bootstrap/Button';
 
 const AddSkills = () => {
 	return (
 		<PageWrapper title={userMenu.user.subMenu.organizationAdmin.text}>
-			<Page>
-				<div className='row d-flex align-items-center h-100'>
-					<div
-						className='col-12 d-flex justify-content-center align-items-center'
-						style={{ fontSize: 'calc(3rem + 3vw)' }}>
-						<Popovers
-							title='AddSkills.tsx'
-							desc={<code>src/pages/presentation/custom/AddSkills.tsx</code>}>
-							AddSkills
-						</Popovers>
-						<code className='ps-3'>AddSkills.tsx</code>
+			<Page className='container'>
+				<div className='mb-lg-5'>
+					<div className='row align-items-center mb-3 mb-lg-5'>
+						<div className='col-lg-12'>
+							<div className='py-3'>
+								<div>SKILLS</div>
+								<div className='display-4 fw-bold'>Add a New Skill</div>
+							</div>
+						</div>
+					</div>
+					<div className='row'>
+						<div className='col-lg-12'>
+							<Card>
+								<CardBody>
+									<div className='row align-items-center'>
+										<div className='col-lg-12'>
+											<FormGroup
+												className='mb-4'
+												id='formPrefix'
+												label='Name'>
+												<Input
+													autoComplete='honorific-prefix'
+													onChange={function noRefCheck() {}}
+													value=''
+													className='w-100'
+												/>
+											</FormGroup>
+										</div>
+									</div>
+									<div className='row align-items-center'>
+										<div className='col-lg-6 mb-2'>
+											<p className='m-0'>Parent</p>
+										</div>
+										<div className='col-lg-12'>
+											<div className='g-drop-search'>
+												<div className='g-drop-search--input'>
+													<p className='m-0'>Sofware Development, Mobile and Web Development <span className='more'>+3 more...</span></p>
+													<Icon
+														icon='KeyboardArrowDown'
+														size='lg'
+														className='ms-4'
+														/>
+												</div>
+												<div className='g-drop-search--content active'>
+													<div className='d-flex align-items-center g-search'>
+														<Icon
+															icon='Search'
+															size='2x'
+															color='dark'
+															/>
+														<Input
+															id="example"
+															onBlur={function noRefCheck(){}}
+															onChange={function noRefCheck(){}}
+															onFocus={function noRefCheck(){}}
+															onInput={function noRefCheck(){}}
+															onInvalid={function noRefCheck(){}}
+															onSelect={function noRefCheck(){}}
+															placeholder="Search"
+															size="lg" 
+															className='ms-2'
+														/>
+													</div>
+													<div className='g-list-check'>
+														<ChecksGroup className='mb-3'>
+															<Checks
+																id="example"
+																label="Computer Science"
+																name="example"
+																onChange={function noRefCheck(){}}
+																type="radio"
+																value="radio value"
+															/>
+															<div className='ms-3'>
+																<Checks
+																	id="example"
+																	label="Sofware Development"
+																	name="example"
+																	onChange={function noRefCheck(){}}
+																	type="radio"
+																	value="radio value"
+																	className=''
+																/>
+															</div>
+															<div className='ms-3'>
+																<Checks
+																	id="example"
+																	label="Mobile and Web Development"
+																	name="example"
+																	onChange={function noRefCheck(){}}
+																	type="radio"
+																	value="radio value"
+																/>
+															</div>
+															<div className='ms-3'>
+																<Checks
+																	id="example"
+																	label="Algoritms"
+																	name="example"
+																	onChange={function noRefCheck(){}}
+																	type="radio"
+																	value="radio value"
+																/>
+															</div>
+														</ChecksGroup>
+														<ChecksGroup>
+															<Checks
+																id="example"
+																label="Computer Science"
+																name="example"
+																onChange={function noRefCheck(){}}
+																type="radio"
+																value="radio value"
+															/>
+															<div className='ms-3'>
+																<Checks
+																	id="example"
+																	label="Sofware Development"
+																	name="example"
+																	onChange={function noRefCheck(){}}
+																	type="radio"
+																	value="radio value"
+																	className=''
+																/>
+															</div>
+															<div className='ms-3'>
+																<Checks
+																	id="example"
+																	label="Mobile and Web Development"
+																	name="example"
+																	onChange={function noRefCheck(){}}
+																	type="radio"
+																	value="radio value"
+																/>
+															</div>
+															<div className='ms-3'>
+																<Checks
+																	id="example"
+																	label="Algoritms"
+																	name="example"
+																	onChange={function noRefCheck(){}}
+																	type="radio"
+																	value="radio value"
+																/>
+															</div>
+														</ChecksGroup>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div className='row align-items-center justify-content-center mt-5'>
+										<div className='col-lg-3'>
+											<Button
+												color='dark'
+												isOutline
+												size='lg'
+												className='w-100 mb-3 mb-lg-0'>
+												Return
+											</Button>
+										</div>
+										<div className='col-lg-3'>
+											<Button
+												color='dark'
+												size='lg'
+												className='w-100 mb-3 mb-lg-0'>
+												Create Skill
+											</Button>
+										</div>
+									</div>
+								</CardBody>
+							</Card>
+						</div>
 					</div>
 				</div>
 			</Page>
--- src/pages/presentation/custom/Categories.tsx
+++ src/pages/presentation/custom/Categories.tsx
@@ -2,22 +2,130 @@ import React from 'react';
 import PageWrapper from '../../../layout/PageWrapper/PageWrapper';
 import Page from '../../../layout/Page/Page';
 import { userMenu } from '../../../menu';
-import Popovers from '../../../components/bootstrap/Popovers';
+import Button from '../../../components/bootstrap/Button';
+import Input from '../../../components/bootstrap/forms/Input';
+import Card, { CardBody } from '../../../components/bootstrap/Card';
+import Icon from '../../../components/icon/Icon';
 
 const Categories = () => {
 	return (
 		<PageWrapper title={userMenu.user.subMenu.organizationAdmin.text}>
-			<Page>
-				<div className='row d-flex align-items-center h-100'>
-					<div
-						className='col-12 d-flex justify-content-center align-items-center'
-						style={{ fontSize: 'calc(3rem + 3vw)' }}>
-						<Popovers
-							title='Categories.tsx'
-							desc={<code>src/pages/presentation/custom/Categories.tsx</code>}>
-							Categories
-						</Popovers>
-						<code className='ps-3'>Categories.tsx</code>
+			<Page className='p-lg-5'>
+				<div className='mb-lg-5'>
+					<div className='row align-items-center mb-3 mb-lg-5'>
+						<div className='col-lg-6'>
+							<div className='display-4 fw-bold py-3'>Categories</div>
+						</div>
+						<div className='col-lg-6'>
+							<div className='row align-items-center justify-content-end'>
+								<div className='col-md-4 col-lg-4'>
+									<Button
+										color="dark"
+										isOutline
+										icon="InsertDriveFile"
+										size='lg'
+										className='w-100 mb-3 mb-lg-0'
+									>
+										Export CSV
+									</Button>
+								</div>
+								<div className='col-md-4 col-lg-4'>
+									<Button 
+										color="dark"
+										icon="Category"
+										size='lg'
+										className='w-100'
+									>
+										Add Category
+									</Button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='row align-items-center'>
+						<div className='col-lg-12'>
+							<Card className='px-4 py-3'>
+								<div className='row align-items-center justify-content-between'>
+									<div className='col-lg-12 mb-2 mb-lg-0'>
+										<div className='d-flex align-items-center'>
+											<Icon
+												icon='Search'
+												size='2x'
+												color='dark'
+												/>
+											<Input
+												id="example"
+												onBlur={function noRefCheck(){}}
+												onChange={function noRefCheck(){}}
+												onFocus={function noRefCheck(){}}
+												onInput={function noRefCheck(){}}
+												onInvalid={function noRefCheck(){}}
+												onSelect={function noRefCheck(){}}
+												placeholder="Search"
+												size="lg" 
+												className='ms-2'
+											/>
+										</div>
+									</div>
+								</div>
+							</Card>
+						</div>
+					</div>
+					<div className='row'>
+						<div className='col-lg-12'>
+							<Card >
+								<CardBody className='table-responsive'>
+									<table className='table table-modern'>
+										<thead>
+											<tr>
+												<th>Name</th>
+												<th>Parent</th>
+												<th></th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>
+													<p className='m-0'>Computer Science</p>
+												</td>
+												<td>
+													<p className='m-0'>None</p>
+												</td>
+												<td className='text-center'>
+													<Button
+														color="dark"
+														isOutline
+														icon="Edit"
+													>
+														Edit
+													</Button>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<p className='m-0'>Computer Science</p>
+												</td>
+												<td>
+													<p className='m-0'>Computer Science</p>
+												</td>
+												<td className='text-center'>
+													<Button
+														color="dark"
+														isOutline
+														icon="Edit"
+													>
+														Edit
+													</Button>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+									<div className='d-flex justify-content-between px-4 mt-4 align-items-center'>
+										<p className='fw-bold m-0'>Showing 1-4 of 10 Categories</p>
+									</div>
+								</CardBody>
+							</Card>
+						</div>
 					</div>
 				</div>
 			</Page>
